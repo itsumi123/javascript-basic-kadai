3c0966346283aaf6b22897f63e60450df6c7855a
itsumi123 <itsumi.726-52442782-@ezweb.ne.jp>
2025-02-07 23:21:22 +0900

    課題をアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,10 @@
+const clickBtn = document.getElementById('btn');
+
+const text = document.getElementById('text');
+
+
+clickBtn.addEventListener('click', () => {
+
+text.textContent = 'ボタンをクリックしました';
+
+});
